c2bd4312dbb24d4474915a302c79a7d72de6ea99
Jacobtread <jacobtread@gmail.com>
2022-05-17 12:09:50 +1200

    Fixed not using async in traits

--- src/tdf.rs
+++ src/tdf.rs
@@ -2,11 +2,11 @@ use std::any::Any;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::io;
-use std::io::{ErrorKind, Read};
+use std::io::{ErrorKind, Read, Write};
 use std::ops::Add;
 use std::string::FromUtf8Error;
 
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
 #[repr(u8)]
 enum TdfType {
@@ -24,13 +24,11 @@ enum TdfType {
 }
 
 
-#[derive(PartialEq)]
 enum MapKey {
     VarInt(u32),
     String(String),
 }
 
-#[derive(PartialEq)]
 enum MapValue {
     VarInt(VarInt),
     String(String),
@@ -38,15 +36,28 @@ enum MapValue {
     Float(f32),
 }
 
-#[repr(u8)]
+#[derive(Clone)]
 enum SubDataType {
-    VarInt = 0x0,
-    String = 0x1,
-    Struct = 0x3,
-    Tripple = 0x9,
-    Float = 0xA,
+    VarInt,
+    String,
+    Struct,
+    Tripple,
+    Float,
 }
 
+impl SubDataType {
+    fn value(&self) -> u8 {
+        match self {
+            SubDataType::VarInt => 0x0,
+            SubDataType::String => 0x1,
+            SubDataType::Struct => 0x3,
+            SubDataType::Tripple => 0x9,
+            SubDataType::Float => 0xA,
+        }
+    }
+}
+
+#[derive( Clone)]
 pub struct VarInt(pub i64);
 
 impl From<usize> for VarInt {
@@ -55,8 +66,10 @@ impl From<usize> for VarInt {
     }
 }
 
+#[derive(Clone)]
 struct LabeledTdf(String, Tdf);
 
+#[derive(Clone)]
 enum Tdf {
     VarInt(VarInt),
     String(String),
@@ -64,51 +77,71 @@ enum Tdf {
     Group(bool, Vec<LabeledTdf>),
     List(SubDataType, Vec<Tdf>),
     Map(SubDataType, SubDataType, Vec<Tdf>, Vec<Tdf>),
-    Union(u8, Option<Tdf>),
+    Union(u8, Option<Box<Tdf>>),
     VarIntList(Vec<VarInt>),
     Pair(VarInt, VarInt),
     Tripple(VarInt, VarInt, VarInt),
     Float(f32),
 }
 
+impl Tdf {
+    fn read<R: Read>(r: &mut R, tdf_type: TdfType) -> io::Result<Self> {
+        match tdf_type {
+            TdfType::VarInt => {}
+            TdfType::String => {}
+            TdfType::Blob => {}
+            TdfType::Group => {}
+            TdfType::List => {}
+            TdfType::Map => {}
+            TdfType::Union => {}
+            TdfType::VarIntList => {}
+            TdfType::Pair => {}
+            TdfType::Tripple => {}
+            TdfType::Float => {}
+        }
+        Ok(Tdf::String(String::from("")))
+    }
+}
+
+
 trait Writeable: Send + Sync {
-    async fn write<W: AsyncWrite>(&self, o: &mut W) -> io::Result<()>;
+    fn write<W: Write>(&self, o: &mut W) -> io::Result<()>;
 }
 
 trait Readable: Send + Sync {
-    async fn read<R: AsyncRead>(r: &mut R) -> io::Result<Self>;
+    fn read<R: Read>(r: &mut R) -> io::Result<Self> where Self: Sized;
 }
 
 impl Writeable for VarInt {
-    async fn write<W: AsyncWrite>(&self, o: &mut W) -> io::Result<()> {
+    fn write<W: Write>(&self, o: &mut W) -> io::Result<()> {
         let value = self.0;
         if value < 0x40 {
-            o.write_u8((value & 0xFF) as u8).await?
+            o.write_u8((value & 0xFF) as u8)?
         } else {
             let mut curr_byte = ((value & 0x3F) as u8) | 0x80;
-            o.write_u8(curr_byte).await?;
+            o.write_u8(curr_byte)?;
             let mut curr_shift = value >> 6;
             while curr_shift >= 0x80 {
                 curr_byte = ((curr_shift & 0x7F) | 0x80) as u8;
                 curr_shift >>= 7;
-                o.write_u8(curr_byte).await?;
+                o.write_u8(curr_byte)?;
             }
-            o.write_u8(curr_shift as u8).await?;
+            o.write_u8(curr_shift as u8)?;
         }
         Ok(())
     }
 }
 
 impl Readable for VarInt {
-    async fn read<R: AsyncRead>(r: &mut R) -> io::Result<VarInt> {
-        let first = r.read_u8().await?;
+    fn read<R: Read>(r: &mut R) -> io::Result<VarInt> {
+        let first = r.read_u8()?;
         let mut shift = 6;
         let mut result = (first & 0x3F) as i64;
         if first >= 0x80 {
             let mut byte: u8;
             loop {
-                byte = r.read_u8().await?;
-                result |= (byte & 0x7F) << shift;
+                byte = r.read_u8()?;
+                result |= ((byte & 0x7F) as i64) << shift;
                 if byte < 0x80 { break; }
             };
         }
@@ -117,11 +150,11 @@ impl Readable for VarInt {
 }
 
 impl Readable for String {
-    async fn read<R: AsyncRead>(r: &mut R) -> io::Result<Self> {
-        let length = VarInt::read(r).await?.0 as usize;
+    fn read<R: Read>(r: &mut R) -> io::Result<Self> {
+        let length = VarInt::read(r)?.0 as usize;
         let mut bytes = vec![0u8; length - 1];
-        r.read_exact(&mut bytes).await?;
-        r.read_u8().await?;
+        r.read_exact(&mut bytes)?;
+        r.read_u8()?;
         match String::from_utf8(bytes) {
             Ok(value) => Ok(value),
             Err(_) => Err(io::Error::new(ErrorKind::InvalidData, "Invalid utf8 string"))
@@ -130,16 +163,16 @@ impl Readable for String {
 }
 
 impl Writeable for String {
-    async fn write<W: AsyncWrite>(&self, o: &mut W) -> io::Result<()> {
+    fn write<W: Write>(&self, o: &mut W) -> io::Result<()> {
         let mut value = self.clone();
         let null_char = char::from(0);
         if value.len() < 1 {
             value.push(null_char)
-        } else if value.chars().last() != null_char {
+        } else if value.chars().last().unwrap() != null_char {
             value.push(null_char)
         }
-        VarInt::from(self.len()).write(o).await?;
-        o.write_all(self.as_bytes()).await?;
+        VarInt::from(self.len()).write(o)?;
+        o.write_all(self.as_bytes())?;
         Ok(())
     }
 }
@@ -148,7 +181,7 @@ impl LabeledTdf {
     fn label_to_tag(label: &String) -> [u8; 3] {
         let mut res = [0u8; 3];
         let buff = label.as_bytes();
-        res[0] |= ((buf[0] & 0x40) << 1);
+        res[0] |= ((buff[0] & 0x40) << 1);
         res[0] |= ((buff[0] & 0x40) << 1);
         res[0] |= ((buff[0] & 0x10) << 2);
         res[0] |= ((buff[0] & 0x0F) << 2);
@@ -170,31 +203,31 @@ impl LabeledTdf {
     fn tag_to_label(tag: u32) -> String {
         let buff: [u8; 4] = tag.to_be_bytes();
         let mut res = [0u8; 4];
-        res[0] |= ((buff[0] & 0x80) >> 1);
-        res[0] |= ((buff[0] & 0x40) >> 2);
-        res[0] |= ((buff[0] & 0x30) >> 2);
-        res[0] |= ((buff[0] & 0x0C) >> 2);
+        res[0] |= (buff[0] & 0x80) >> 1;
+        res[0] |= (buff[0] & 0x40) >> 2;
+        res[0] |= (buff[0] & 0x30) >> 2;
+        res[0] |= (buff[0] & 0x0C) >> 2;
 
-        res[1] |= ((buff[0] & 0x02) << 5);
-        res[1] |= ((buff[0] & 0x01) << 4);
-        res[1] |= ((buff[1] & 0xF0) >> 4);
+        res[1] |= (buff[0] & 0x02) << 5;
+        res[1] |= (buff[0] & 0x01) << 4;
+        res[1] |= (buff[1] & 0xF0) >> 4;
 
-        res[2] |= ((buff[1] & 0x08) << 3);
-        res[2] |= ((buff[1] & 0x04) << 2);
-        res[2] |= ((buff[1] & 0x03) << 2);
-        res[2] |= ((buff[2] & 0xC0) >> 6);
+        res[2] |= (buff[1] & 0x08) << 3;
+        res[2] |= (buff[1] & 0x04) << 2;
+        res[2] |= (buff[1] & 0x03) << 2;
+        res[2] |= (buff[2] & 0xC0) >> 6;
 
-        res[3] |= ((buff[2] & 0x20) << 1);
+        res[3] |= (buff[2] & 0x20) << 1;
         res[3] |= (buff[2] & 0x1F);
 
         return buff.iter()
-            .map(|v| if v == 0 { char::from(0x20) } else { char::from(*v) })
+            .map(|v| if *v == 0 { char::from(0x20) } else { char::from(*v) })
             .collect::<String>();
     }
 }
 
 impl Writeable for LabeledTdf {
-    async fn write<W: AsyncWrite>(&self, o: &mut W) -> io::Result<()> {
+    fn write<W: Write>(&self, o: &mut W) -> io::Result<()> {
         let tdf_type = match self.1 {
             Tdf::VarInt(_) => TdfType::VarInt,
             Tdf::String(_) => TdfType::String,
@@ -208,69 +241,71 @@ impl Writeable for LabeledTdf {
             Tdf::Tripple(_, _, _) => TdfType::Tripple,
             Tdf::Float(_) => TdfType::Float
         };
-        let tag = LabeledTdf::label_to_tag(label);
-        o.write_u8(tag[0]).await?;
-        o.write_u8(tag[1]).await?;
-        o.write_u8(tag[2]).await?;
-        o.write_u8(tdf_type as u8).await?;
-        self.1.write(o).await?;
+        let tag = LabeledTdf::label_to_tag(&self.0);
+        o.write_u8(tag[0])?;
+        o.write_u8(tag[1])?;
+        o.write_u8(tag[2])?;
+        o.write_u8(tdf_type as u8)?;
+        self.1.write(o)?;
         Ok(())
     }
 }
 
+
 impl Writeable for Tdf {
-    async fn write<W: AsyncWrite>(&self, o: &mut W) -> io::Result<()> {
+    fn write<W: Write>(&self, o: &mut W) -> io::Result<()> {
         match self {
-            Tdf::VarInt(value) => value.write(o).await?,
-            Tdf::String(value) => value.write(o).await?,
-            Tdf::Blob(value)=> o.write_all(value).await?,
+            Tdf::VarInt(value) => value.write(o)?,
+            Tdf::String(value) => value.write(o)?,
+            Tdf::Blob(value) => o.write_all(value)?,
             Tdf::Group(start2, values) => {
-                if start2 { o.write_u8(2).await?; }
+                if *start2 { o.write_u8(2)?; }
                 for value in values {
-                    value.write(o).await?;
+                    value.write(o)?;
                 }
             }
             Tdf::List(sub_type, values) => {
-                o.write_u8(sub_type as u8).await?;
-                VarInt::from(values.len()).write(o).await?;
-                values.iter()
-                    .for_each(|v| v.write(o, false));
+                o.write_u8(sub_type.value())?;
+                VarInt::from(values.len()).write(o)?;
+                for value in values {
+                    value.write(o)?;
+                }
             }
             Tdf::Map(key_type, value_type, keys, values) => {
-                o.write_u8(key_type as u8).await?;
-                o.write_u8(value_type as u8).await?;
+                o.write_u8(key_type.value())?;
+                o.write_u8(value_type.value())?;
                 let length = keys.len();
                 for i in 0..(length - 1) {
                     let key = keys.get(i).unwrap();
                     let value = values.get(i).unwrap();
-                    key.write(o).await?;
-                    value.write(o).await?;
+                    key.write(o)?;
+                    value.write(o)?;
                 }
             }
             Tdf::Union(data, value) => {
-                o.write_u8(*data).await?;
-                if data != 0x7F && value.is_some() {
-                    value.unwrap().write(o).await?;
+                o.write_u8(*data)?;
+                if *data != 0x7F {
+                    let v = value.as_ref().unwrap().as_ref();
+                    v.write(o)?;
                 }
             }
             Tdf::VarIntList(values) => {
-                VarInt::from(values.len()).write(o).await?;
+                VarInt::from(values.len()).write(o)?;
                 for value in values {
-                    value.write(o).await?;
+                    value.write(o)?;
                 }
             }
             Tdf::Pair(a, b) => {
-                a.write(o).await?;
-                b.write(o).await?;
+                a.write(o)?;
+                b.write(o)?;
             }
             Tdf::Tripple(a, b, c) => {
-                a.write(o).await?;
-                b.write(o).await?;
-                c.write(o).await?;
+                a.write(o)?;
+                b.write(o)?;
+                c.write(o)?;
             }
-            Tdf::Float(value) => o.write_f32(*value).await?,
+            Tdf::Float(value) => o.write_f32::<BigEndian>(*value)?,
         }
         Ok(())
     }
 }
-
